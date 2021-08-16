f5bd9217ec911db1daa6df217cd06b4cea682aad
NineRogues <alexander.eric.3@gmail.com>
2021-08-15 20:34:02 -0500

    arreglos

--- core/settings.py
+++ core/settings.py
@@ -148,3 +148,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
